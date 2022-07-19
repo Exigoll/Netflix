63f935aaa2f24a1eafadcd61a44ae18d1bcedfb6
Exigoll <exigoll@bk.ru>
2022-07-19 22:34:29 +0500

    feat: pf + styles for Header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,37 @@
-import styles from "./Header.module.scss";
+import { BellIcon, SearchIcon } from "@heroicons/react/outline";
+import Link from "next/link";
 
 const Header = () => {
   return (
-    <div>
-      <h1>Header</h1>
-    </div>
+    <header>
+      <div className="flex items-center space-x-2 md:space-x-10">
+        <img src="https://rb.gy/ulxxee"
+             alt="Netflix logo"
+             width={100}
+             height={100}
+             className="cursor-pointer object-contain" />
+        <ul className="hidden space-x-4 md:flex">
+          <li className="headerLink">Home</li>
+          <li className="headerLink">TV Shows</li>
+          <li className="headerLink">Movies</li>
+          <li className="headerLink">New & Popular</li>
+          <li className="headerLink">My list</li>
+        </ul>
+      </div>
+      <div className="flex items-center space-x-4 text-sm font-light">
+        <SearchIcon className="hidden sm:inline"
+                    width={20}
+                    height={20} />
+        <p className="hidden lg:inline">Kids</p>
+        <BellIcon width={20}
+                  height={20} />
+        <Link href="/account">
+          <img className="cursor-pointer rounded"
+               src="https://rb.gy/g1pwyx"
+               alt="" />
+        </Link>
+      </div>
+    </header>
   );
 };
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,28 @@
-import Head from "next/head";
+import type { NextPage } from "next";
 
+import Head from "next/head";
 import Header from "../components/Header";
 
-
-const Home = (): JSX.Element => {
+const Home: NextPage = () => {
   return (
-    <div className="container">
+    <div className="flex min-h-screen flex-col items-center justify-center py-2">
       <Head>
-        <title>Create Next App</title>
+        <title>Netflix: Home page</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
-
+      <main>
+        {/* Banner */}
+        <section>
+          {/* Row */}
+          {/* Row */}
+          {/* Row */}
+          {/* Row */}
+          {/* Row */}
+          {/* Row */}
+        </section>
+      </main>
+      {/* Modal */}
     </div>
   );
 };
