0b29322fa36645eb06e7c6cb5acc517f6e714349
Exigoll <exigoll@bk.ru>
2022-07-25 10:24:04 +0500

    feat: page Login

--- hooks/useAuth.tsx
+++ hooks/useAuth.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-
+import { useRouter } from "next/router";
+import { auth } from "../firebase";
 import {
   createUserWithEmailAndPassword,
   onAuthStateChanged,
@@ -8,13 +9,106 @@ import {
   User,
 } from "firebase/auth";
 
-import { useRouter } from "next/router";
-import { auth } from "../firebase";
+interface IAuth {
+  user: User | null;
+  signUp: (email: string, password: string) => Promise<void>;
+  signIn: (email: string, password: string) => Promise<void>;
+  logout: () => Promise<void>;
+  error: string | null;
+  loading: boolean;
+}
 
-const useAuth = () => {
-  const [user, setUser] = React.useState();
+const AuthContext = React.createContext<IAuth>({
+  user: null,
+  signUp: async () => {
+  },
+  signIn: async () => {
+  },
+  logout: async () => {
+  },
+  error: null,
+  loading: false,
+});
 
-  return user;
+interface AuthProviderProps {
+  children: React.ReactNode;
+}
+
+export const AuthProvider = ({ children }: AuthProviderProps) => {
+  const [loading, setLoading] = React.useState(false);
+  const [user, setUser] = React.useState<User | null>(null);
+  const [error, setError] = React.useState(null);
+  const [initialLoading, setInitialLoading] = React.useState(true);
+  const router = useRouter();
+
+  React.useEffect(
+    () =>
+      onAuthStateChanged(auth, (user) => {
+        if (user) {
+          // Logged in...
+          setUser(user);
+          setLoading(false);
+        } else {
+          // Not logged in...
+          setUser(null);
+          setLoading(true);
+          router.push("/login");
+        }
+
+        setInitialLoading(false);
+      }),
+    [auth],
+  );
+
+
+  const signUp = async (email: string, password: string) => {
+    setLoading(true);
+
+    await createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        setUser(userCredential.user);
+        router.push("/");
+        setLoading(false);
+      })
+      .catch((error) => alert(error.message))
+      .finally(() => setLoading(false));
+  };
+
+  const signIn = async (email: string, password: string) => {
+    setLoading(true);
+
+    await signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        setUser(userCredential.user);
+        router.push("/");
+        setLoading(false);
+      })
+      .catch((error) => alert(error.message))
+      .finally(() => setLoading(false));
+  };
+
+  const logout = async () => {
+    setLoading(true);
+    signOut(auth)
+      .then(() => {
+        setUser(null);
+      })
+      .catch((error) => alert(error.message))
+      .finally(() => setLoading(false));
+  };
+
+  const memoedValue = React.useMemo(() => ({
+    user, signUp, signIn, loading, logout, error,
+  }), [user, loading]);
+
+  return (
+    <AuthContext.Provider value={memoedValue}>
+      {!initialLoading && children}
+    </AuthContext.Provider>
+  );
 };
 
-export default useAuth;
+// eslint-disable-next-line require-jsdoc
+export default function useAuth() {
+  return React.useContext(AuthContext);
+}
